7336d5819147d1743a856b8a6a96013c43a0fb02
Josiah Purtlebaugh <josiah.purtlebaugh@gmail.com>
2014-07-15 06:53:54 -0700

    Added authentication to the queries and sorted the gist output.

--- cache.js
+++ cache.js
@@ -4,6 +4,9 @@ var redis     = require('redis'),
      rsub     = redis.createClient(),
      docstore = redis.createClient();
 
+console.log('Loading configuration...');
+var config = require('./codelove-community.json');
+
 rsub.subscribe('codelove-community');
 rsub.on('message', function(channel, message) {
   if (channel == 'codelove-community') {
@@ -40,7 +43,7 @@ function cacheUsers() {
     },
     hostname: 'api.github.com',
     method: 'GET', 
-    path: '/orgs/codelove-org/public_members',
+    path: '/orgs/codelove-org/public_members?client_id=' + config.client_id + '&client_secret=' + config.client_secret,
     port: '443',
     rejectUnauthorized: 'false',
     requestCert: 'true'
@@ -68,6 +71,9 @@ function cacheGists() {
       var member = members[i];
 
       if (!member) {
+        gists = gists.sort(function(a,b) {
+          return new Date(a.updated_at) - new Date(b.updated_at);
+        }).reverse();
         docstore.set('codelove-community.gists', JSON.stringify(gists), redis.print); 
         return;
       }
@@ -79,7 +85,7 @@ function cacheGists() {
         },
         hostname: 'api.github.com',
         method: 'GET', 
-        path: '/users/' + member.login + '/gists',
+        path: '/users/' + member.login + '/gists?client_id=' + config.client_id + '&client_secret=' + config.client_secret,
         port: '443',
         rejectUnauthorized: 'false',
         requestCert: 'true'
